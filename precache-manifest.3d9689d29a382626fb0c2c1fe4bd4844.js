a8a95f7914a0953584dbadc211f63eacfce6220c
Sherwin Aborot <saborot@gmail.com>
2019-07-24 12:30:14 +0800

    Updates

---
+++ precache-manifest.3d9689d29a382626fb0c2c1fe4bd4844.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "853ab664ac444bfda89456bcca13aef2",
+    "url": "/flames/index.html"
+  },
+  {
+    "revision": "f9cc2c714e20161df037",
+    "url": "/flames/static/css/main.da00fb50.chunk.css"
+  },
+  {
+    "revision": "54fabe2828d821e2f6b0",
+    "url": "/flames/static/js/2.07df93b6.chunk.js"
+  },
+  {
+    "revision": "f9cc2c714e20161df037",
+    "url": "/flames/static/js/main.078ebcd3.chunk.js"
+  },
+  {
+    "revision": "490c8007674ee981eb87",
+    "url": "/flames/static/js/runtime~main.c80e6c8b.js"
+  }
+]);
